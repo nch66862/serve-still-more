8fc20468d6d5782b9ca3992854c8dabb63de1b9c
Nick Carver <nicholascarver2@outlook.com>
2021-03-16 14:49:01 -0500

    the page loads again! No errors. After adding form components

--- src/Components/auth/Register.js
+++ src/Components/auth/Register.js
@@ -1,78 +1,175 @@
-import React, { useRef } from "react"
+import React, { useState } from "react"
 import { useHistory } from "react-router-dom"
+import { states } from "../Settings"
+import { authApi, userStorageKey } from "./authSettings"
 import "./Register.css"
 
-export const Register = (props) => {
-    const firstName = useRef()
-    const lastName = useRef()
-    const email = useRef()
-    const conflictDialog = useRef()
+export const Register = () => {
     const history = useHistory()
+    let stateCounter = 0
+    const [currentPage, setCurrentPage] = useState("first")
+    const [registerUser, setRegisterUser] = useState({
+        firstName: "",
+        lastName: "",
+        email: "",
+        password: "",
+        groupId: 0,
+        role: 0,
+        phone: "",
+        address: "",
+        city: "",
+        state: "",
+        photo: "",
+        callTime: "",
+        canCall: false,
+        familyId: "",
+        primaryMember: false
+    })
+    const [conflictDialog, setConflictDialog] = useState(false)
+
+    const handleInputChange = (event) => {
+        const newUser = { ...registerUser }
+        newUser[event.target.id] = event.target.value
+        setRegisterUser(newUser)
+    }
 
     const existingUserCheck = () => {
-        return fetch(`http://localhost:8088/users?email=${email.current.value}`)
+        return fetch(`${authApi.localApiBaseUrl}/${authApi.endpoint}?email=${registerUser.email}`)
             .then(res => res.json())
             .then(user => !!user.length)
     }
 
-    const handleRegister = (e) => {
-        e.preventDefault()
+    const nextPage = (event) => {
+        event.preventDefault()
+        setCurrentPage("second")
+    }
 
-
+    const handleRegister = (event) => {
+        event.preventDefault()
         existingUserCheck()
             .then((userExists) => {
                 if (!userExists) {
-                    fetch("http://localhost:8088/users", {
+                    fetch(`${authApi.localApiBaseUrl}/${authApi.endpoint}`, {
                         method: "POST",
                         headers: {
                             "Content-Type": "application/json"
                         },
-                        body: JSON.stringify({
-                            email: email.current.value,
-                            name: `${firstName.current.value} ${lastName.current.value}`
-                        })
+                        body: JSON.stringify(registerUser)
                     })
                         .then(res => res.json())
                         .then(createdUser => {
                             if (createdUser.hasOwnProperty("id")) {
-                                localStorage.setItem("Lost_River_User", createdUser.id)
+                                sessionStorage.setItem(userStorageKey, createdUser.id)
                                 history.push("/")
                             }
                         })
                 }
                 else {
-                    conflictDialog.current.showModal()
+                    setConflictDialog(true)
                 }
             })
-        
     }
 
-    return (
-        <main style={{ textAlign: "center" }}>
-            <dialog className="dialog dialog--password" ref={conflictDialog}>
-                <div>Account with that email address already exists</div>
-                <button className="button--close" onClick={e => conflictDialog.current.close()}>Close</button>
-            </dialog>
+    const FirstPage = () => {
+        return (
+            <main style={{ textAlign: "center" }}>
+                <dialog className="dialog dialog--password" open={conflictDialog}>
+                    <div>Account with that email address already exists</div>
+                    <button className="button--close" onClick={e => setConflictDialog(false)}>Close</button>
+                </dialog>
+                <h1>Lost River Call Center</h1>
+                <form className="form--login" onSubmit={nextPage}>
+                    <h1 className="h3 mb-3 font-weight-normal">Sign Up</h1>
+                    <fieldset>
+                        <label htmlFor="firstName"> First Name </label>
+                        <input onChange={handleInputChange} type="text" name="firstName" className="form-control" placeholder="first name" value={registerUser.firstName} id="firstName" required autoFocus />
+                    </fieldset>
+                    <fieldset>
+                        <label htmlFor="lastName"> Last Name </label>
+                        <input onChange={handleInputChange} type="text" name="lastName" className="form-control" placeholder="last name" value={registerUser.lastName} id="lastName" required />
+                    </fieldset>
+                    <fieldset>
+                        <label htmlFor="inputEmail"> Email address </label>
+                        <input onChange={handleInputChange} type="email" name="email" className="form-control" placeholder="email address" value={registerUser.email} id="email" required />
+                    </fieldset>
+                    <fieldset>
+                        <label htmlFor="inputPassword"> Create Password </label>
+                        <input onChange={handleInputChange} type="password" name="email" className="form-control" placeholder="create password" value={registerUser.password} id="password" required />
+                    </fieldset>
+                    <fieldset>
+                        <button type="submit"> Next </button>
+                    </fieldset>
+                </form>
+            </main>
+        )
+    }
 
-            <form className="form--login" onSubmit={handleRegister}>
-                <h1 className="h3 mb-3 font-weight-normal">Please Register for NSS Kennels</h1>
-                <fieldset>
-                    <label htmlFor="firstName"> First Name </label>
-                    <input ref={firstName} type="text" name="firstName" className="form-control" placeholder="First name" required autoFocus />
-                </fieldset>
-                <fieldset>
-                    <label htmlFor="lastName"> Last Name </label>
-                    <input ref={lastName} type="text" name="lastName" className="form-control" placeholder="Last name" required />
-                </fieldset>
-                <fieldset>
-                    <label htmlFor="inputEmail"> Email address </label>
-                    <input ref={email} type="email" name="email" className="form-control" placeholder="Email address" required />
-                </fieldset>
-                <fieldset>
-                    <button type="submit"> Sign in </button>
-                </fieldset>
-            </form>
-        </main>
-    )
+    const SecondPage = () => {
+        return (
+            <main style={{ textAlign: "center" }}>
+                <h1>Lost River Call Center</h1>
+                <h3>A Little More Information Is Needed</h3>
+                <form className="form--login" onSubmit={handleRegister}>
+                    <h1 className="h3 mb-3 font-weight-normal">Sign Up</h1>
+                    <fieldset>
+                        <label htmlFor="firstName"> Group Number </label>
+                        <select onChange={handleInputChange} value={registerUser.locationId} name="locationId" id="locationId" className="form-control" >
+                            <option value="0">Select a location</option>
+                            {states.map(state => {
+                                stateCounter++
+                                return <option key={stateCounter} value={stateCounter}>{state}</option>
+                            })}
+                        </select>
+                    </fieldset>
+                    <fieldset>
+                        <label htmlFor="lastName"> Role </label>
+                        <select onChange={handleInputChange} value={registerUser.locationId} name="locationId" id="locationId" className="form-control" >
+                            <option value="0">Select a location</option>
+                            {states.map(state => {
+                                stateCounter++
+                                return <option key={stateCounter} value={stateCounter}>{state}</option>
+                            })}
+                        </select>
+                    </fieldset>
+                    <fieldset>
+                        <label htmlFor="inputEmail"> Phone Number </label>
+                        <input onChange={handleInputChange} value={registerUser.locationId} type="email" name="email" className="form-control" placeholder="9805554466" required />
+                    </fieldset>
+                    <fieldset>
+                        <label htmlFor="inputEmail"> Address </label>
+                        <input onChange={handleInputChange} value={registerUser.locationId} type="email" name="email" className="form-control" placeholder="address" required />
+                    </fieldset>
+                    <fieldset>
+                        <label htmlFor="inputEmail"> City </label>
+                        <input onChange={handleInputChange} value={registerUser.locationId} type="email" name="email" className="form-control" placeholder="city" required />
+                    </fieldset>
+                    <fieldset>
+                        <label htmlFor="inputEmail"> State </label>
+                        <select onChange={handleInputChange} value={registerUser.locationId} name="locationId" id="locationId" className="form-control" >
+                            <option value="0">Select a State</option>
+                            {states.map(state => {
+                                stateCounter++
+                                return <option key={stateCounter} value={stateCounter}>{state}</option>
+                            })}
+                        </select>
+                    </fieldset>
+                    <fieldset>
+                        <button type="submit"> Next </button>
+                    </fieldset>
+                </form>
+            </main>
+        )
+    }
+
+    if (currentPage === "first") {
+        return (
+            <FirstPage />
+        )
+    }
+    else if (currentPage === "second") {
+        return (
+            <SecondPage />
+        )
+    }
 }
 
