fb59ac1f323e310d418706b4976dd6651b1c04ff
Nick Carver <nicholascarver2@outlook.com>
2021-03-16 09:43:59 -0500

    built initial file structure and deleted app.js. Main component is now ServeStillMore

---
+++ src/Components/ServeStillMore.js
@@ -0,0 +1,5 @@
+export const ServeStillMore = () => {
+    return (
+        <h1>Lost River Call Center</h1>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ServeStillMore } from './Components/ServeStillMore';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ServeStillMore />
   </React.StrictMode>,
   document.getElementById('root')
 );
