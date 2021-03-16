9714c4778da8d4d2dd83bd116d37715a21a85ef7
Nick Carver <nicholascarver2@outlook.com>
2021-03-16 10:50:59 -0500

    got the router set up to route me in if I am logged in

--- src/Components/ServeStillMore.js
+++ src/Components/ServeStillMore.js
@@ -1,5 +1,27 @@
-export const ServeStillMore = () => {
-    return (
-        <h1>Lost River Call Center</h1>
-    )
-}
+import React from "react";
+import { Route, Redirect } from "react-router-dom";
+import { Register } from "./auth/Register";
+import "./ServeStillMore.css";
+
+export const ServeStillMore = () => (
+    <>
+        <Route
+            render={() => {
+                if (localStorage.getItem("Lost_River_User")) {
+                    return (
+                        <h1>I am Logged In</h1>
+                    );
+                } else {
+                    return <Redirect to="/login" />;
+                }
+            }}
+        />
+
+        <Route path="/login">
+            <Register />
+        </Route>
+        <Route path="/register">
+            <Register />
+        </Route>
+    </>
+)
