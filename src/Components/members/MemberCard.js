348871f73c60516d300c4ca3d8e8a15d9ba2d664
Nick Carver <nicholascarver2@outlook.com>
2021-03-19 10:52:23 -0500

    got the member detail card to show up if a member is clicked on. Shows up in line with the rest of the names and only the name is currently displayed.

--- src/Components/members/MemberCard.js
+++ src/Components/members/MemberCard.js
@@ -1,10 +1,16 @@
-import { Link } from 'react-router-dom'
+import { useState } from 'react'
 import './MemberCard.css'
+import { MemberDetail } from './MemberDetail'
 
 export const MemberCard = ({ member }) => {
+    const [openDetail, setOpenDetail] = useState(false)
+    const handleClickMember = () => {
+        setOpenDetail(true)
+    }
     return (
-        <Link to={`/members/detail/${member.id}`}>
-            <h5 className="personCard">{member.firstName} {member.lastName}</h5>
-        </Link>
+        <>
+            <h5 onClick={handleClickMember} className="personCard">{member.firstName} {member.lastName}</h5>
+            {openDetail && <MemberDetail setOpenDetail={setOpenDetail} member={member} />}
+        </>
     )
 }
--- src/Components/members/MemberDetail.js
+++ src/Components/members/MemberDetail.js
@@ -1,20 +1,9 @@
-import { useContext, useEffect, useState } from "react"
-import { useParams } from "react-router"
-import { MemberContext } from "./MemberProvider"
 
-export const MemberDetail = () => {
+export const MemberDetail = ({ member }) => {
 
-    const [member, setMember] = useState({})
-    const { getMemberById } = useContext(MemberContext)
-    const { memberId } = useParams()
-
-    useEffect(() => {
-        getMemberById(memberId)
-        .then(setMember)
-    }, [])
     return (
         <>
-        <h1>{member?.firstName} {member?.lastName}</h1>
+            <h1>{member.firstName} {member.lastName}</h1>
         </>
     )
 }
