b29167ace5b89f5b21a5cb8f6ac8d080ee6e7be8
Nick Carver <nicholascarver2@outlook.com>
2021-03-19 10:07:31 -0500

    put together singlee item in member detail using params. Ready to set up detail address

--- src/Components/members/MemberDetail.js
+++ src/Components/members/MemberDetail.js
@@ -1,6 +1,20 @@
+import { useContext, useEffect, useState } from "react"
+import { useParams } from "react-router"
+import { MemberContext } from "./MemberProvider"
+
 export const MemberDetail = () => {
+
+    const [member, setMember] = useState({})
+    const { getMemberById } = useContext(MemberContext)
+    const { memberId } = useParams()
+
+    useEffect(() => {
+        getMemberById(memberId)
+        .then(setMember)
+    }, [])
     return (
         <>
+        <h1>{member?.firstName} {member?.lastName}</h1>
         </>
     )
 }
