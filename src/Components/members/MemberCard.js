664fc7e1f2cb01e5badf5197468ae6bbd68cf130
Nick Carver <nicholascarver2@outlook.com>
2021-03-19 10:22:26 -0500

    added a link to the detail of each member. But now thinking about it. I dont think it is the right move

--- src/Components/members/MemberCard.js
+++ src/Components/members/MemberCard.js
@@ -1,7 +1,10 @@
+import { Link } from 'react-router-dom'
 import './MemberCard.css'
 
 export const MemberCard = ({ member }) => {
     return (
-        <h5 className="personCard">{member.firstName} {member.lastName}</h5>
+        <Link to={`/members/detail/${member.id}`}>
+            <h5 className="personCard">{member.firstName} {member.lastName}</h5>
+        </Link>
     )
 }
