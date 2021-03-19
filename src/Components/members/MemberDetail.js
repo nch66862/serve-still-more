621a0ced3b85e50aca005d60b1be684abe6bb2e6
Nick Carver <nicholascarver2@outlook.com>
2021-03-19 11:09:36 -0500

    turned the member detail into a modal

--- src/Components/members/MemberDetail.js
+++ src/Components/members/MemberDetail.js
@@ -1,12 +1,15 @@
 
 export const MemberDetail = ({ member, setOpenDetail }) => {
-const handleClose = () => {
-    setOpenDetail(false)
-}
+    const handleClose = () => {
+        setOpenDetail(false)
+    }
+
     return (
-        <>
-            <h1>{member.firstName} {member.lastName}</h1>
-            <button onClick={handleClose}>close</button>
-        </>
+        <main className="modal--parent">
+            <section className="modal--content">
+                <h1>{member.firstName} {member.lastName}</h1>
+                <button onClick={handleClose}>close</button>
+            </section>
+        </main>
     )
 }
