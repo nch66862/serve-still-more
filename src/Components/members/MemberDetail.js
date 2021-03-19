0db57ae3a34ecef1df418cd4102224082dbe906b
Nick Carver <nicholascarver2@outlook.com>
2021-03-19 11:03:55 -0500

    added a functional close button to member detail

--- src/Components/members/MemberDetail.js
+++ src/Components/members/MemberDetail.js
@@ -1,9 +1,12 @@
 
-export const MemberDetail = ({ member }) => {
-
+export const MemberDetail = ({ member, setOpenDetail }) => {
+const handleClose = () => {
+    setOpenDetail(false)
+}
     return (
         <>
             <h1>{member.firstName} {member.lastName}</h1>
+            <button onClick={handleClose}>close</button>
         </>
     )
 }
