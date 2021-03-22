575697ff8cd6bad6a7a0c15cbba8c4b417504b24
Nick Carver <nicholascarver2@outlook.com>
2021-03-22 15:16:58 -0500

    added memberdetail to the deacon dashboard

--- src/Components/users/DeaconDashboard.js
+++ src/Components/users/DeaconDashboard.js
@@ -1,17 +1,32 @@
 import { useState } from "react"
 import { CallForm } from "../history/CallForm"
+import { MemberDetail } from "../members/MemberDetail"
 import { News } from "../news/News"
 import './DeaconDashboard.css'
 
 export const DeaconDashboard = () => {
 
     const member = {
-        id: 1
+        "firstName": "Jeremy Boy",
+        "lastName": "Jones",
+        "email": "",
+        "groupId": 1,
+        "phone": "(270) 543-3086",
+        "address": "435 Main Street",
+        "city": "Nashville",
+        "state": "TN",
+        "photo": "",
+        "callTime": "",
+        "canCall": true,
+        "familyId": 0,
+        "primaryMember": false,
+        "id": 1
     }
 
     return (
         <main className="elderDashboard">
             <section className="leftContent">
+                <MemberDetail member={member}/>
                 <CallForm member={member}/>
             </section>
             <section className="rightContent">
