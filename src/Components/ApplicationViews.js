8f7215307ded69f41976a2d0a34e6e229170247d
Nick Carver <nicholascarver2@outlook.com>
2021-03-25 16:49:10 -0500

    finished commenting code. removed images directory that was empty

--- src/Components/ApplicationViews.js
+++ src/Components/ApplicationViews.js
@@ -9,21 +9,21 @@ import { HistoryProvider } from "./history/HistoryProvider";
 import { ElderDashboard } from "./users/ElderDashboard";
 import { DeaconDashboard } from "./users/DeaconDashboard";
 import { UserContext, UserProvider } from "./users/UserProvider";
-
+//this component handles which component to display based on the URL route
 export const ApplicationViews = () => {
-
+    //gets the current logged in user id
     const loggedInUserId = parseInt(sessionStorage.getItem("Lost_River_User"))
     const [loggedInUserObj, setLoggedInUserObj] = useState({})
     const { getUserById } = useContext(UserContext)
     const { getRoles, roles } = useContext(RoleContext)
+    //finds the matching role object of the current logged in user
     const matchingRole = roles?.find(role => role.id === loggedInUserObj.roleId)
-
+    //gets data and sets the current logged in user state variable
     useEffect(() => {
         getUserById(loggedInUserId)
             .then(setLoggedInUserObj)
             .then(getRoles)
     }, [])
-
     return (
         <>
             <Route exact path="/">
@@ -33,6 +33,7 @@ export const ApplicationViews = () => {
                             <NewsProvider>
                                 <MemberProvider>
                                     <HistoryProvider>
+                                        {/* renders a dashboard based on if the user is an elder or not */}
                                         {matchingRole?.name.toLowerCase() === "elder" ? <ElderDashboard /> : <DeaconDashboard />}
                                     </HistoryProvider>
                                 </MemberProvider>
--- src/Components/users/UserProvider.js
+++ src/Components/users/UserProvider.js
@@ -2,17 +2,17 @@ import React, { useState, createContext } from "react"
 
 // The context is imported and used by individual components that need data
 export const UserContext = createContext()
-
 // This component establishes what data can be used.
 export const UserProvider = (props) => {
+    //main state variable that stores all of the user data from the api
     const [users, setUsers] = useState([])
-
+    //gets all of the user objects
     const getUsers = () => {
         return fetch("http://localhost:8088/users")
             .then(res => res.json())
             .then(setUsers)
     }
-
+    //creates a user obj in the database
     const addUser = userObj => {
         return fetch("http://localhost:8088/users", {
             method: "POST",
@@ -23,12 +23,12 @@ export const UserProvider = (props) => {
         })
         .then(getUsers)
     }
-
+    //fetches a single user object from the database
     const getUserById = (id) => {
         return fetch(`http://localhost:8088/users/${id}`)
             .then(res => res.json())
     }
-
+    //replaces the infomation in an object with different information
     const updateUser = userObj => {
         return fetch(`http://localhost:8088/users/${userObj.id}`, {
             method: "PUT",
@@ -39,14 +39,14 @@ export const UserProvider = (props) => {
         })
             .then(getUsers)
     }
-
+    //deletes a user object in the database
     const deleteUser = userId => {
         return fetch(`http://localhost:8088/users/${userId}`, {
             method: "DELETE",
         })
             .then(getUsers)
     }
-
+    //allows access to things in the provider through the context
     return (
         <UserContext.Provider value={{
             users, getUsers, addUser, getUserById, updateUser, deleteUser
