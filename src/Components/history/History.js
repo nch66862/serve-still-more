13e7eefcb0c283ee7f861e37eb02f3b498473eb6
Nick Carver <nicholascarver2@outlook.com>
2021-03-25 11:45:10 -0500

    added more coments and a header for history and improved the conditional rendering in history

--- src/Components/history/History.js
+++ src/Components/history/History.js
@@ -1,11 +1,13 @@
 import { useContext, useEffect } from "react"
 import { HistoryContext } from "./HistoryProvider"
 import './History.css'
-
+//component that displays the call history for a certain user
 export const History = ({ member }) => {
-
+    //allows access to functions and data in the history provider
     const { getHistory, history } = useContext(HistoryContext)
+    //finds the call logs that match the currently selected member to call
     const historyForMember = history.filter(history => history.memberId === member?.id)
+    //sorts the call logs by date posted
     const sortedHistory = historyForMember.slice().sort((a, b) => {
         const aDate = new Date(a.date)
         const bDate = new Date(b.date)
@@ -13,24 +15,26 @@ export const History = ({ member }) => {
         b.date = bDate
         return b.date - a.date
     })
-
+    //does the fetch call to get the history log for all users
     useEffect(() => {
         getHistory()
     }, [])
-
+    //maps through the sorted history and builds out the date and who called them and the notes that were made
     return (
-        <section className="historyList">
-            <h3>Conversation History</h3>
-            {sortedHistory.map(history => {
-                return (
-                    <section key={history.id}>
-                        <label>{history.date.toLocaleDateString('en-US', { timeZone: "CST" })} - {history.user.firstName} {history.user.lastName}</label>
-                        <article className="historyCard" >
-                            <label>{history.note}</label>
-                        </article>
-                    </section>
-                )
-            })}
-        </section>
+        <>
+            {Object.keys(member).length !== 0 && <section className="historyList">
+                <h3>Conversation History</h3>
+                {sortedHistory.map(history => {
+                    return (
+                        <section key={history.id}>
+                            <label>{history?.date.toLocaleDateString('en-US', { timeZone: "CST" })} - {history.user.firstName} {history.user.lastName}</label>
+                            <article className="historyCard" >
+                                <label>{history.note}</label>
+                            </article>
+                        </section>
+                    )
+                })}
+            </section>}
+        </>
     )
 }
