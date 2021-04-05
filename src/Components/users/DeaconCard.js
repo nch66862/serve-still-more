5adbf60888874e114d4139947c00eca3da31496c
Nick Carver <nicholascarver2@outlook.com>
2021-04-05 14:08:01 -0500

    got the deacon cards to display the photos of the members

--- src/Components/users/DeaconCard.js
+++ src/Components/users/DeaconCard.js
@@ -1,7 +1,18 @@
 import './UserCard.css'
+import { Image } from 'cloudinary-react'
+
 //component that shows the basic information for a single deacon
 export const DeaconCard = ({ user }) => {
+    let croppedURLPhoto = ""
+    if (user.photo) {
+        const [prefix, suffix] = user.photo.split("/upload/")
+        const cropSection = "/upload/w_400,h_400,c_crop,g_face,r_max/w_100/"
+        croppedURLPhoto = prefix.concat(cropSection, suffix)
+    }
     return (
-        <h5 className="userCard">{user.firstName} {user.lastName}</h5>
+        <article className="deaconCard">
+            <Image style={{ borderRadius: "50px", marginRight: "10px", marginLeft: "10px", marginBottom: "5px" }} cloudName="nch66862" publicId={croppedURLPhoto} />
+            <h5 className="userCard">{user.firstName} {user.lastName}</h5>
+        </article>
     )
 }
